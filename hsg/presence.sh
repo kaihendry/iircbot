646bd8eda46995aaa1f6d0e1469314bf2ce3952b
Kai Hendry <hendry@webconverger.com>
2013-04-02 05:01:41 +0000

    Epic sed

--- hsg/presence.sh
+++ hsg/presence.sh
@@ -1,4 +1,5 @@
-tail -f /var/log/freeradius/radacct/49.128.60.116/detail-20130401 | while read -r foo
+#cat /var/log/freeradius/radacct/49.128.60.116/detail-$(date +%Y%m%d) | while read -r foo
+tail -f /var/log/freeradius/radacct/49.128.60.116/detail-$(date +%Y%m%d) | while read -r foo
 do
 case $foo in
 Framed-IP-Address*)
@@ -8,10 +9,18 @@ Acct-Status-Type*)
 	line="$line $(echo $foo | cut -d" " -f3)"
 	;;
 Calling-Station-Id*)
-	line="$line $(echo $foo | cut -d" " -f3)"
+	mac=$(sed -e 's/\(..\)\(..\)\.\(..\)\(..\)\.\(..\)\(..\)/\1:\2:\3:\4:\5:\6/' <<< $(echo $foo | cut -d" " -f3 | tr -d '"'))
+	desc=$(grep $mac presence.table)
+	if test "$desc"
+	then
+	line="$line $desc"
+	else
+	line="$line $mac"
+	fi
 	;;
 *)
 	;;
 esac
 test "$foo" || echo $line >> ~/irc/irc.freenode.net/#hackerspacesg/in
+#test "$foo" || echo $line
 done
