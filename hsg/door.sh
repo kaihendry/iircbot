74048ceef994a39fc9b90b8c209ff7c729fc548d
Kai Hendry <hendry@webconverger.com>
2013-04-02 04:40:29 +0000

    Make line less long

--- hsg/door.sh
+++ hsg/door.sh
@@ -1,1 +1,2 @@
-tail -n 0 -f /srv/www/hsgdoor.codersg.com/logs/hsgdoor-entry.log | stdbuf -o0 sed 's/^\[.*\]://' > ~/irc/irc.freenode.net/#hackerspacesg/in
+tail -n 0 -f /srv/www/hsgdoor.codersg.com/logs/hsgdoor-entry.log |
+stdbuf -o0 sed 's/^\[.*\]://' > ~/irc/irc.freenode.net/#hackerspacesg/in
