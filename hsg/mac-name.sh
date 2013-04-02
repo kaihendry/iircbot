0b804d067a49aa298342eba6d9347c1410b5ed65
Kai Hendry <hendry@webconverger.com>
2013-04-02 12:38:42 +0800

    build out table

--- hsg/mac-name.sh
+++ hsg/mac-name.sh
@@ -3,16 +3,21 @@
 
 
 #cat report-eg | while read -r report
-nmap -sP 192.168.5.* | while read -r report
+sudo nmap -sP 192.168.5.1/24 | while read -r report
 do
 	case $report in
 	"Nmap scan report for"*)
 		read -r _ _ _ _ IP _ <<< "$report"
-		read -r _ name <<< "$(avahi-resolve-address $IP 2>/dev/null)"
+		read -r _ name <<< "$(avahi-resolve-address "$IP" 2>/dev/null)"
 		;;
 	"MAC Address"*)
 		read -r _ _ mac _ _ _ <<< "$report"
-		echo $mac $name
+		if test "$name"
+		then
+			echo "$mac" "$name"
+		else
+			echo "$report" | sed 's,^MAC Address: ,,'
+		fi
 		;;
 	*)
 		;;
